ed14c32b8f2e92ab612dd0ef61b817937bc1ab19
{hui lin chu} <3a832008@gm.student.ncut.edu.tw>
2021-11-22 22:48:11 +0800

    練習4-2將新增文章儲存路由

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,7 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('posts'          , ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
     Route::get('posts/create'   , ['as' => 'admin.posts.create' , 'uses' => 'AdminPostsController@create']);
     Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'   , 'uses' => 'AdminPostsController@edit']);
+    Route::post('posts',[AaminPostController::class,'store'])->name('admin.posts.store');
 });
+
+
